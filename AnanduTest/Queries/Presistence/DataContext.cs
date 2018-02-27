98149fa0532a3cffa8498ae3092f9ad4b0e581a8
anandu nv <ananduadv@gmail.com>
2018-02-27 12:44:36 +0530

    cc

---
+++ AnanduTest/Queries/Presistence/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries.Presistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("name=ConnectionName")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+
+        }
+
+        //dbset here
+        //  public virtual DbSet<Customer> Customers { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //EntityTypeConfiguration setter here
+            //   modelBuilder.Configurations.Add(new CustomerConfiguration());
+
+        }
+    }
+}
---
+++ AnanduTest/Queries/Presistence/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using Queries.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queries.Presistence
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly DataContext _context;
+
+        public UnitOfWork(DataContext context)
+        {
+            _context = context;
+
+            // Assign Context for repositories here
+            // Customers = new CustomerRepository(_context);
+
+        }
+
+        // proprty for customer repositories
+        //public ICustomerRepository Customers { get; private set; }
+
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
